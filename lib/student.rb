b86bddc382eed6952202b341285fdc9f42db2069
Miranda Delaney <delaney.miranda1@gmail.com>
2023-09-06 11:32:40 -0400

    feat: Methods to complete iteration 1

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
 class Student
-  
+  attr_reader :name, :age, :scores
+  def initialize(student_info)
+    @name = student_info[:name]
+    @age = student_info[:age]
+    @scores = []
+  end
+
+  def log_score(test_score)
+    @scores << test_score
+  end
+
+  def grade
+    @scores.sum.to_f / @scores.length
+  end
 end
